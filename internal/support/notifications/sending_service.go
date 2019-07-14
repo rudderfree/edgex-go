91e1d6550d5c0e6b5063180c99bebb9aa922aaa8
Amit Kumar Chaudhary <a.rudder.free@gmail.com>
2019-07-14 18:43:03 +0530

    Add support for skiping cert verification for self-signed
    This commit adds the re-implementation of smtp.SendMail function
    to enable skipping the verification of invalid (self-signed)
    certs.
    
    This commit only adds the foundation for implementing this feature,
    not the feature itself. With this commit existing functions are
    expected to remain as is and none added.
    
    Signed-off-by: Amit Kumar Chaudhary <a.rudder.free@gmail.com>

--- internal/support/notifications/sending_service.go
+++ internal/support/notifications/sending_service.go
@@ -17,6 +17,9 @@ package notifications
 
 import (
 	"bytes"
+	"crypto/tls"
+	"errors"
+	"net"
 	"net/http"
 	mail "net/smtp"
 	"strconv"
@@ -90,9 +93,9 @@ func smtpSend(message string, addressees []string) models.TransmissionRecord {
 	// required CRLF at ends of lines and CRLF between header and body for SMTP RFC 822 style email
 	buf.WriteString("\r\n")
 	buf.WriteString(message)
-	err := mail.SendMail(smtp.Host+":"+strconv.Itoa(smtp.Port),
+	err := sendMail(smtp.Host+":"+strconv.Itoa(smtp.Port),
 		mail.PlainAuth("", smtp.Sender, smtp.Password, smtp.Host),
-		smtp.Sender, addressees, []byte(buf.String()))
+		smtp.Sender, addressees, []byte(buf.String()), false)
 	if err != nil {
 		LoggingClient.Error("Problems sending message to: " + strings.Join(addressees, ",") + ", issue: " + err.Error())
 		tr.Status = models.Failed
@@ -134,4 +137,71 @@ func handleFailedTransmission(t models.Transmission) {
 			}
 		}
 	}
+}
+
+// validateLine checks to see if a line has CR or LF as per RFC 5321
+func validateLine(line string) error {
+	if strings.ContainsAny(line, "\n\r") {
+		return errors.New("smtp: A line must not contain CR or LF")
+	}
+	return nil
+}
+
+func sendMail(addr string, a mail.Auth, from string, to []string, msg []byte, skipVerify bool) error {
+	if err := validateLine(from); err != nil {
+		return err
+	}
+	for _, recp := range to {
+		if err := validateLine(recp); err != nil {
+			return err
+		}
+	}
+	c, err := mail.Dial(addr)
+	if err != nil {
+		return errors.New("Notifications: Address dialing error")
+	}
+	serverName, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	if err = c.Hello(addr); err != nil {
+		return err
+	}
+	if ok, _ := c.Extension("STARTTLS"); ok {
+		config := &tls.Config{ServerName: serverName}
+		config.InsecureSkipVerify = skipVerify
+		if err = c.StartTLS(config); err != nil {
+			return err
+		}
+	}
+	if a != nil {
+		if ok, _ := c.Extension("AUTH"); !ok {
+			return errors.New("smtp: server doesn't support AUTH")
+		}
+		if err = c.Auth(a); err != nil {
+			return err
+		}
+	}
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
 }
